28c4c8948e3161ef56d65152785654fb8bacc5a4
Your Name <you@example.com>
2015-09-21 16:02:29 +0300

    fixed sh script

--- 4_5/five.sh
+++ 4_5/five.sh
@@ -3,4 +3,5 @@
 mkdir to/{a..z}
 for i in {a..z};do mv *$i* ../to/${i:0:1}; done;
 cd ../to/;
-for i in {a..z};do cd $i/; for s in *$i_*; do mv $s ${s/$i_};done; done;
+for i in {a..z}; do cd $i/; for s in *$i_*; do mv $s ${s/${s:0:2}}; done  ;cd ../; done;
+
