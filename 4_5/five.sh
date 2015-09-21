30b154f76c3e68a0ca89c0caa09519c1da84e772
Your Name <you@example.com>
2015-09-21 16:44:35 +0300

    fixed moving files to /to directory

--- 4_5/five.sh
+++ 4_5/five.sh
@@ -1,7 +1,6 @@
 команды в консоли:
 
 mkdir to/{a..z}
-for i in {a..z};do mv *$i* ../to/${i:0:1}; done;
+for i in {a..z}; do if [ -e $i* ]; then mv *$i* ../to/${i:0:1}; fi;done;
 cd ../to/;
-for i in {a..z}; do cd $i/; for s in *$i_*; do mv $s ${s/${s:0:2}}; done  ;cd ../; done;
-
+for i in {a..z}; do cd $i/; for s in *$i_*; do if [ $s != "**" ];then mv $s ${s/${s:0:2}}; fi  ;done  ;cd ../; done;
